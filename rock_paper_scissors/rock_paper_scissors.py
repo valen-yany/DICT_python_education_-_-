d408572041e608c35f207f5f04fa8869ab1c23b8
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-04-07 17:18:33 +0300

    Rock Paper Scissors 2-nd stage

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -1,17 +1,31 @@
+import random
+
 class RPS:
     def __init__(self):
-        self.option_list = ['rock', 'paper', 'scissors']
+        self.__option_list = ['rock', 'paper', 'scissors']
         self.__game()
+
+    def __computer_choice(self):
+        return random.choice(self.__option_list)
 
     def __player_turn(self):
         player_choice = input('>')
-        while player_choice not in self.option_list:
+        while player_choice not in self.__option_list:
             player_choice = input('Choice the correct option:\n>')
         return player_choice
 
+    def __result(self, player, computer):
+        if player == computer:
+            print(f'There is a draw({computer})')
+        elif player == self.__option_list[self.__option_list.index(computer) - 1]:
+            print(f'Sorry, but the computer chose {computer}')
+        else:
+            print(f'Well done. The computer chose {computer} and failed')
+
     def __game(self):
         player_choice = self.__player_turn()
-        print(f'Sorry, but the computer chose {self.option_list[self.option_list.index(player_choice) - 2]}')
+        computer_choice = self.__computer_choice()
+        self.__result(player_choice, computer_choice)
 
 
 play = RPS()
