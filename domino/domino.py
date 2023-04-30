4f221cc76315ae3c217c717c62f2b4350d6910d2
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-04-30 20:31:24 +0300

    Domino 4-th stage

--- domino/domino.py
+++ domino/domino.py
@@ -1,70 +1,158 @@
 import random
 
 
-def game():
-    stack = domino_create()
-    player_hand = []
-    computer_hand = []
-    reserve = []
-    status = 0
-    turn = ''
-    message = ''
-    first_turn = []
-    snake = []
-    while status == 0:
-        reserve = stack[:]
-        player_hand = new_hand(reserve)
-        computer_hand = new_hand(reserve)
-        status, first_turn = status_check(player_hand, computer_hand)
-    if status == 1:
-        turn = 'player'
-        message = 'It\'s your turn to make a move. Enter your command.'
-        player_hand.remove(first_turn)
-    else:
-        turn = 'computer'
-        message = 'Computer is about to make a move. Press Enter to continue...'
-        computer_hand.remove(first_turn)
-    snake += first_turn
-    player_hand_formatted = [f'{num}: {piece}' for num, piece in enumerate(player_hand)]
-    print(f'''{'=' * 70}
-Stock size: {len(reserve)}
-Computer pieces: {len(computer_hand)}
+class Domino:
+    def __init__(self):
+        self.stack = []
+        self.snake = []
+        self.player = ('Player', 'PC')
+        self.message = ('It\'s your turn to make a move. Enter your command:\n',
+                        'Computer is about to make a move. Please wait...\n')
+        self.turn = -1
+        while self.turn == -1:
+            self.domino_create()
+            self.player_hand = self.new_hand()
+            self.comp_hand = self.new_hand()
+            self.status_check()
+        self.game()
 
-Snake: {snake}
+    def domino_create(self):
+        for i in range(0, 7):
+            for j in range(i, 7):
+                self.stack.append([i, j])
+
+    def new_hand(self):
+        hand = []
+        for i in range(0, 7):
+            hand.append(random.choice(self.stack))
+            self.stack.remove(hand[-1])
+        return hand
+
+    def status_check(self):
+        first_doubles = sorted([i for i in self.player_hand if i[0] == i[1]], key=lambda x: x[0], reverse=True)
+        second_doubles = sorted([i for i in self.comp_hand if i[0] == i[1]], key=lambda x: sum(x), reverse=True)
+        if len(first_doubles) != 0 and len(second_doubles) != 0:
+            if sum(first_doubles[0]) > sum(second_doubles[0]):
+                self.turn = 1
+                self.snake.append(first_doubles[0])
+                self.player_hand.remove(first_doubles[0])
+            elif sum(second_doubles[0]) > sum(first_doubles[0]):
+                self.turn = 0
+                self.snake.append(second_doubles[0])
+                self.comp_hand.remove(second_doubles[0])
+        elif len(first_doubles) == 0 and len(second_doubles) != 0:
+            self.turn = 0
+            self.snake.append(second_doubles[0])
+            self.comp_hand.remove(second_doubles[0])
+        elif len(second_doubles) == 0 and len(first_doubles) != 0:
+            self.turn = 1
+            self.snake.append(first_doubles[0])
+            self.player_hand.remove(first_doubles[0])
+
+    def player_turn(self):
+        while True:
+            turn = int(input())
+            if turn > len(self.player_hand) or turn < -len(self.player_hand):
+                print('Illegal move')
+            elif turn == 0:
+                if len(self.stack) > 0:
+                    self.player_hand.append(random.choice(self.stack))
+                    self.stack.remove(self.player_hand[-1])
+                break
+            elif turn > 0:
+                if self.snake[-1][1] == self.player_hand[turn-1][0]:
+                    self.snake += [self.player_hand[turn-1]]
+                    self.player_hand.remove(self.player_hand[turn-1])
+                    break
+                elif self.snake[-1][1] == self.player_hand[turn-1][1]:
+                    self.snake += [self.player_hand[turn - 1][::-1]]
+                    self.player_hand.remove(self.player_hand[turn - 1])
+                    break
+                else:
+                    print('Illegal move')
+            elif turn < 0:
+                if self.snake[0][0] == self.player_hand[-turn-1][1]:
+                    self.snake = [self.player_hand[-turn-1]] + self.snake
+                    self.player_hand.remove(self.player_hand[-turn - 1])
+                    break
+                elif self.snake[0][0] == self.player_hand[-turn-1][0]:
+                    self.snake = [self.player_hand[-turn - 1][::-1]] + self.snake
+                    self.player_hand.remove(self.player_hand[-turn - 1])
+                    break
+                else:
+                    print('Illegal move')
+
+    def check_draw(self):
+        snake = ''
+        for i in self.snake:
+            snake += ''.join(list(map(str, i)))
+        if snake.count(snake[0]) == 8 and snake.count(snake[-1]) == 8:
+            return True
+        return False
+
+    def comp_turn(self):
+        turns = [i for i in range(-len(self.comp_hand), len(self.comp_hand) + 1) if i != 0]
+        while len(turns) > 0:
+            turn = random.choice(turns)
+            if turn > 0:
+                if self.snake[-1][1] == self.comp_hand[turn-1][0]:
+                    self.snake += [self.comp_hand[turn-1]]
+                    self.comp_hand.remove(self.comp_hand[turn-1])
+                    break
+                elif self.snake[-1][1] == self.comp_hand[turn-1][1]:
+                    self.snake += [self.comp_hand[turn - 1][::-1]]
+                    self.comp_hand.remove(self.comp_hand[turn - 1])
+                    break
+                else:
+                    turns.remove(turn)
+                    continue
+            elif turn < 0:
+                if self.snake[0][0] == self.comp_hand[-turn-1][1]:
+                    self.snake = [self.comp_hand[-turn-1]] + self.snake
+                    self.comp_hand.remove(self.comp_hand[-turn - 1])
+                    break
+                elif self.snake[0][0] == self.comp_hand[-turn-1][0]:
+                    self.snake = [self.comp_hand[-turn - 1][::-1]] + self.snake
+                    self.comp_hand.remove(self.comp_hand[-turn - 1])
+                    break
+                else:
+                    turns.remove(turn)
+                    continue
+        if len(self.stack) > 0:
+            self.comp_hand.append(random.choice(self.stack))
+            self.stack.remove(self.comp_hand[-1])
+
+    def game(self):
+        status = self.message[self.turn]
+        while True:
+            player_hand_formatted = [f'{num+1}: {piece}' for num, piece in enumerate(self.player_hand)]
+            print(f'''{'=' * 70}
+Stock size: {len(self.stack)}
+Computer pieces: {len(self.comp_hand)}
+
+Snake: {self.snake}
 
 Your pieces: ''')
-    for i in player_hand_formatted:
-        print(i)
-    print(f"\nStatus: {message}")
+            for i in player_hand_formatted:
+                print(i)
+            print(f"\nStatus: {status}")
+            if status not in self.message:
+                break
+            if self.player[self.turn] == 'Player':
+                self.player_turn()
+                if len(self.player_hand) == 0:
+                    status = 'The game is over. You won!'
+                    continue
+            elif self.player[self.turn] == 'PC':
+                self.comp_turn()
+                if len(self.comp_hand) == 0:
+                    status = 'The game is over. The computer won!'
+                    continue
+            if self.check_draw():
+                status = 'The game is over. It\'s a draw!'
+                continue
+            self.turn = abs(self.turn - 1)
+            status = self.message[self.turn]
 
-def domino_create():
-    domino_stack = []
-    for i in range(0, 7):
-        for j in range(i, 7):
-            domino_stack.append((i, j))
-    return domino_stack
 
-
-def new_hand(stack):
-    hand = []
-    for i in range(0, 7):
-        hand.append(random.choice(stack))
-        stack.remove(hand[-1])
-    return hand
-
-
-def status_check(first_hand, second_hand):
-    first_doubles = sorted([i for i in first_hand if i[0] == i[1]], key=lambda x: x[0], reverse=True)
-    second_doubles = sorted([i for i in second_hand if i[0] == i[1]], key=lambda x: sum(x), reverse=True)
-    if len(first_doubles) != 0 and len(second_doubles) != 0:
-        if sum(first_doubles[0]) > sum(second_doubles[0]):
-            return 1, first_doubles[0]
-        if sum(second_doubles[0]) > sum(first_doubles[0]):
-            return 2, second_doubles[0]
-    if len(first_doubles) == 0:
-        return 2, second_doubles[0]
-    if len(second_doubles) == 0:
-        return 1, first_doubles[0]
-    return 0, None
-
-game()
+D = Domino()
