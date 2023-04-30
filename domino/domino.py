6e9f9bc2424e9c2fd6e249d57c1cc51aa4d01994
valen-yany <v.v.yanytskyi@student.khai.edu>
2023-04-30 18:21:14 +0300

    Domino 3-rd stage

--- domino/domino.py
+++ domino/domino.py
@@ -1,37 +1,6 @@
 import random
 
 
-def domino_create():
-    domino_stack = []
-    for i in range(0, 7):
-        for j in range(i, 7):
-            domino_stack.append((i, j))
-    return domino_stack
-
-
-def new_hand(stack):
-    hand = []
-    for i in range(0, 7):
-        hand.append(random.choice(stack))
-        stack.remove(hand[-1])
-    return hand
-
-
-def status_check(first_hand, second_hand):
-    first_doubles = sorted([i for i in first_hand if i[0] == i[1]], key=lambda x: x[0], reverse=True)
-    second_doubles = sorted([i for i in second_hand if i[0] == i[1]], key=lambda x: sum(x), reverse=True)
-    if len(first_doubles) != 0 and len(second_doubles) != 0:
-        if sum(first_doubles[0]) > sum(second_doubles[0]):
-            return 1, first_doubles[0]
-        if sum(second_doubles[0]) > sum(first_doubles[0]):
-            return 2, second_doubles[0]
-    if len(first_doubles) == 0:
-        return 2, second_doubles[0]
-    if len(second_doubles) == 0:
-        return 1, first_doubles[0]
-    return 0, None
-
-
 def game():
     stack = domino_create()
     player_hand = []
@@ -68,5 +37,34 @@ Your pieces: ''')
         print(i)
     print(f"\nStatus: {message}")
 
+def domino_create():
+    domino_stack = []
+    for i in range(0, 7):
+        for j in range(i, 7):
+            domino_stack.append((i, j))
+    return domino_stack
+
+
+def new_hand(stack):
+    hand = []
+    for i in range(0, 7):
+        hand.append(random.choice(stack))
+        stack.remove(hand[-1])
+    return hand
+
+
+def status_check(first_hand, second_hand):
+    first_doubles = sorted([i for i in first_hand if i[0] == i[1]], key=lambda x: x[0], reverse=True)
+    second_doubles = sorted([i for i in second_hand if i[0] == i[1]], key=lambda x: sum(x), reverse=True)
+    if len(first_doubles) != 0 and len(second_doubles) != 0:
+        if sum(first_doubles[0]) > sum(second_doubles[0]):
+            return 1, first_doubles[0]
+        if sum(second_doubles[0]) > sum(first_doubles[0]):
+            return 2, second_doubles[0]
+    if len(first_doubles) == 0:
+        return 2, second_doubles[0]
+    if len(second_doubles) == 0:
+        return 1, first_doubles[0]
+    return 0, None
 
 game()
